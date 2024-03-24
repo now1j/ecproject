d47e22d08603416ad79a03cf187a51cb1c6614ab
now1j <149587520+now1j@users.noreply.github.com>
2024-03-24 14:19:49 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/ecproject/EcprojectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.ecproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/ecproject/EcprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.ecproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
