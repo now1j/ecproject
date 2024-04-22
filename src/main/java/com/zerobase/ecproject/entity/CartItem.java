164433cdf6a1e1d36555333eea0cbec9f54cfa7b
now1j <149587520+now1j@users.noreply.github.com>
2024-04-22 13:09:02 +0900

    Initial commit

--- src/main/java/com/zerobase/ecproject/entity/CartItem.java
+++ src/main/java/com/zerobase/ecproject/entity/CartItem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
@@ -13,6 +14,9 @@ public class CartItem {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToOne
+  private Member member;
 
   @ManyToOne
   private Product product;
